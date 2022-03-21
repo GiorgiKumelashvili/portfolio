06001ba0943088027b00eb6b60b978fe0812d6da
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2022-03-21 23:30:30 +0400

    card section done

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -2,6 +2,9 @@
 // https://github.com/michalsnik/aos/tree/v2
 // https://michalsnik.github.io/aos/
 
+// navbar underline must be implemented like bottom link
+// http://rainbowit.net/themes/trydo/personal-portfolio-02/#
+
 const x: number = 123;
 
 console.log(123);
