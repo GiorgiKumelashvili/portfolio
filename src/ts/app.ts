a8f770bd82e00119db5ab68f5346c8334bbc60a1
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2022-03-25 00:03:46 +0400

    mvp done

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -11,13 +11,13 @@
  * (+) CV (plus inside about me)
  * (+) correct social links on right
  * (+) mail on old left implement
- * ( ) image in about me
- * ( ) projects
+ * (+) image in about me
+ * (+) projects
  *
  * responsivnes (fuuuuuuck !!!)
  */
 
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener('DOMContentLoaded', () => {
 	document.getElementById('nav-second-section').addEventListener('click', () => {
 		document.getElementById('second-section').scrollIntoView({ behavior: 'smooth' });
 	});
@@ -27,4 +27,27 @@ window.addEventListener('DOMContentLoaded', event => {
 	document.getElementById('nav-fourth-section').addEventListener('click', () => {
 		document.getElementById('fourth-section').scrollIntoView({ behavior: 'smooth' });
 	});
+});
+
+const navbar = document.getElementById('header');
+var lastScrollTop = 0;
+
+window.addEventListener('scroll', () => {
+	var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+
+	if (st > lastScrollTop) {
+		// downscroll code
+		navbar.classList.add('scroll');
+	} else {
+		// upscroll code
+		navbar.classList.remove('scroll');
+	}
+
+	if (window.scrollY < 20) {
+		navbar.classList.remove('scroll-background');
+	} else {
+		navbar.classList.add('scroll-background');
+	}
+
+	lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
 });
